86058de3236f79a3209b0814b1a42a2392258048
Julian Möwenmann <moewenmann@protonmail.ch>
2026-03-01 07:35:49 +0100

    moved to template for proxy config; renamed proxy setup script; updated default config template to redirect to the frontend

--- proxy/proxy-setup.sh
+++ proxy/proxy-setup.sh
@@ -4,8 +4,8 @@ echo "Proxy-Setup: running certificate setup..."
 set -e
 
 CERT_DIR="/etc/nginx/certs"
-CERT_FILE="$CERT_DIR/transcendence-selfsigned.crt"
-KEY_FILE="$CERT_DIR/transcendence-selfsigned.key"
+CERT_FILE="$CERT_DIR/${TLS_CERT_FILE}"
+KEY_FILE="$CERT_DIR/${TLS_KEY_FILE}"
 
 mkdir -p $CERT_DIR
 
@@ -17,10 +17,17 @@ if [ ! -f "$CERT_FILE" ] || [ ! -f "$KEY_FILE" ]; then
     -out $CERT_FILE \
     -subj "/C=DE/ST=BW/L=HN/O=transcendence/CN=localhost"
 
+  chmod 600 "$KEY_FILE"
   echo "Proxy-Setup: certificate generated."
 else
   echo "Proxy-Setup: certificate already exists."
 fi
+
+echo "Proxy-Setup: generating nginx config from template..."
+TEMPLATE="/etc/nginx/templates/default.conf.template"
+TARGET="/etc/nginx/conf.d/default.conf"
+envsubst '${PROXY_HTTP_PORT} ${PROXY_HTTPS_PORT} ${TLS_CERT_FILE} ${TLS_KEY_FILE} ${FRONTEND_PORT}' \
+  < "$TEMPLATE" > "$TARGET"
 
 echo "Proxy-Setup: complete. Start nginx..."
 exec "$@"
