7ea83789b442afa1f1b1d3689db102b0a65f12b6
Balint Szikora <chamaselion@gmail.com>
2026-03-29 14:20:30 +0200

    Switched back to old version, after learning proper env setup :D

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from '@prisma/client';
-// import { Pool } from 'pg';
+import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { DATABASE_URL } from './config.js';
 
-// const pool = new Pool({ connectionString: DATABASE_URL });
-// const adapter = new PrismaPg(pool);
+const pool = new Pool({ connectionString: DATABASE_URL });
+const adapter = new PrismaPg(pool);
 
-const adapter = new PrismaPg({ connectionString: DATABASE_URL });
+// const adapter = new PrismaPg({ connectionString: DATABASE_URL });
 
 export const prisma = new PrismaClient({ adapter });
