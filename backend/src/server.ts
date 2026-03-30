8eec9195b37a987b249f30d3dfd75fc192a70892
Kirila Ruseva <kira.ruseva@gmail.com>
2026-03-30 12:52:23 +0200

    server logic enhanced for socket.io requiring a handshake and verify token

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,7 @@ import { Server as SocketIOServer } from 'socket.io';
 import { APP_ORIGIN, FRONTEND_ORIGIN, PORT } from './config.js';
 import { createApp } from './app.js';
 import { initFileStorage } from './files/storage.js';
+import { verifyAccessToken } from './auth/jwt.js';
 
 export function startServer() {
   initFileStorage();
@@ -11,26 +12,43 @@ export function startServer() {
   const httpServer = createServer(app);
 
   const io = new SocketIOServer(httpServer, {
-  path: '/socket.io',
-  cors: {
-	origin: [APP_ORIGIN, FRONTEND_ORIGIN],
-	credentials: true,
-  },
+    path: '/socket.io',
+    cors: {
+      origin: [APP_ORIGIN, FRONTEND_ORIGIN],
+      credentials: true,
+    },
+  });
+
+  io.use((socket, next) => {
+    const token = socket.handshake.auth?.token;
+
+    if (!token) {
+      return next(new Error('No token provided'));
+    }
+
+    try {
+      const user = verifyAccessToken(token);
+      (socket as any).user = user;
+      next();
+    } catch (err) {
+      return next(new Error('Unauthorized'));
+    }
   });
 
   io.on('connection', (socket) => {
-	console.log('socket connected:', socket.id);
-
-	socket.on('chat:message', (payload: { text: string }) => {
-		io.emit('chat:message', { ...payload, from :socket.id});
-	});
-
-	socket.on('disconnect', () => {
-		console.log('socket disconnected:', socket.id);
-	});
+    const user = (socket as any).user;
+    console.log('Authenticated user connected:', user);
+    socket.on('chat:message', (payload: { text: string }) => {
+      io.emit('chat:message', { ...payload, from: socket.id });
+    });
+    socket.on('disconnect', () => {
+      console.log('socket disconnected:', socket.id);
+    });
+    socket.emit('welcome', `Hello ${user.username}`);
   });
 
   httpServer.listen(PORT, () => {
     console.log(`Backend Svr listening on http://localhost:${PORT}`);
   });
+  return io;
 }
