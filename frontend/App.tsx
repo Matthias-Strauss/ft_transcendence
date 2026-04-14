0370f5c4645c0365918cafec4f32b465cd2d9d4e
Kirila Ruseva <kira.ruseva@gmail.com>
2026-04-14 20:35:02 +0200

    resolving issue with sidebar + css migration process for chat

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -34,15 +34,6 @@ export default function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegistrationPage />} />
         <Route
-          path="/game"
-          element={
-            <ProtectedRoute>
-              <PongGame />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
           path="/"
           element={
             <ProtectedRoute>
