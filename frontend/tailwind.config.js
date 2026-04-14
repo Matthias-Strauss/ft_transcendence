1c95a1d5d4e07bb6dc8ad28de73beb34d9c5adc2
Kirila Ruseva <kira.ruseva@gmail.com>
2026-04-14 20:58:23 +0200

    applying partial css migration and correct config files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './App.{js,ts,jsx,tsx}',
+    './SocialApp.{js,ts,jsx,tsx}',
+    './main.{js,ts,jsx,tsx}',
+    './api/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './context/**/*.{js,ts,jsx,tsx}',
+    './game/**/*.{js,ts,jsx,tsx}',
+    './hooks/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './types/**/*.{js,ts,jsx,tsx}',
+    './utils/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
